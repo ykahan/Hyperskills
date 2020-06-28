8143ffe25764e4334f1894f979f2475b14311d6b
Jatudrei <yehoshua.kahan@gmail.com>
2020-06-28 14:37:57 +0300

    Moved messages to user to Messages class

--- src/CoffeeMachine/Messages.java
+++ src/CoffeeMachine/Messages.java
@@ -1,4 +1,12 @@
 package CoffeeMachine;
 
 public class Messages {
+    static void notPositiveIntegerError() {
+        System.out.println("Error: Not a positive integer");
+    }
+
+    static void amountsRequest(String material, String units) {
+        System.out.println("How many " + units + " of " + material + "?");
+        System.out.println("Please make sure to enter a positive integer.");
+    }
 }
